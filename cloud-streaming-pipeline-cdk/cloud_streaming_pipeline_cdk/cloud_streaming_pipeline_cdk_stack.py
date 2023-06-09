fe86abc874715706ce8052b523123c341dd4c960
Tim Sagona <tim.sagona@gmail.com>
2023-06-09 07:07:46 -0500

    Initial commit of creation of S3 buckets for handles

--- cloud-streaming-pipeline-cdk/cloud_streaming_pipeline_cdk/cloud_streaming_pipeline_cdk_stack.py
+++ cloud-streaming-pipeline-cdk/cloud_streaming_pipeline_cdk/cloud_streaming_pipeline_cdk_stack.py
@@ -3,8 +3,9 @@ from aws_cdk import (
     # Duration,
     aws_dynamodb as ddb,
     aws_kinesis as kinesis,
-    Stack
+    Stack,
     # aws_sqs as sqs,
+    aws_s3 as s3
 )
 
 from constructs import Construct
@@ -16,7 +17,7 @@ class CloudStreamingPipelineCdkStack(Stack):
         
         # Create a new DynamoDB table
         self.dynamo_table = ddb.Table(
-            self, "id123",
+            self, "dynamo-customers",
             table_name = "customers",
             partition_key=ddb.Attribute(
                 name="customer_id",
@@ -25,14 +26,9 @@ class CloudStreamingPipelineCdkStack(Stack):
             billing_mode=ddb.BillingMode.PAY_PER_REQUEST
         )
         
-
-        # Create a new Kinesis stream
-        #The id123 is the id of the kinesis stream. Not sure on much more than that. 
-        self.kinesis_stream = kinesis.Stream(
-            self, "id123",
-            shard_count=1,  # Start with 1 shard
-            stream_name="Customers",
-            retention_period=cdk.Duration.days(1)
-        )
-        # s3 bucket
+        # s3 bucket for each handle in handles file
+        with open('handles.txt') as file:
+            handles = file.read().splitlines()
+            for handle in handles:
+                s3.Bucket(self, handle, bucket_name='hwe-' + handle, removal_policy=cdk.RemovalPolicy.DESTROY)
         # athena metastore
