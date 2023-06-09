ccacf154037c0be14d8c1a16c27f38d0ebfdfef1
Tim Sagona <tim.sagona@gmail.com>
2023-06-09 09:33:57 -0500

    Initial commit of Athena resources

--- cloud-streaming-pipeline-cdk/cloud_streaming_pipeline_cdk/cloud_streaming_pipeline_cdk_stack.py
+++ cloud-streaming-pipeline-cdk/cloud_streaming_pipeline_cdk/cloud_streaming_pipeline_cdk_stack.py
@@ -5,7 +5,9 @@ from aws_cdk import (
     aws_kinesis as kinesis,
     Stack,
     # aws_sqs as sqs,
-    aws_s3 as s3
+    aws_s3 as s3,
+    aws_glue as glue,
+    aws_athena as athena
 )
 
 from constructs import Construct
@@ -16,6 +18,7 @@ class CloudStreamingPipelineCdkStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         
         # Create a new DynamoDB table
+        # If this table already exists, deploy will fail.
         self.dynamo_table = ddb.Table(
             self, "dynamo-customers",
             table_name = "customers",
@@ -31,4 +34,32 @@ class CloudStreamingPipelineCdkStack(Stack):
             handles = file.read().splitlines()
             for handle in handles:
                 s3.Bucket(self, handle, bucket_name='hwe-' + handle, removal_policy=cdk.RemovalPolicy.DESTROY)
-        # athena metastore
+
+
+        # Create an S3 bucket for Athena query results
+        # If this bucket already exists, deploy will fail.
+        s3.Bucket(self, 'hwe-athena-query-results', bucket_name='hwe-athena-query-results', removal_policy=cdk.RemovalPolicy.DESTROY)
+
+        # Create an Athena WorkGroup
+        workgroup = athena.CfnWorkGroup(
+            self,
+            "hwe-athena-workgroup",
+            name="hwe",
+            work_group_configuration=athena.CfnWorkGroup.WorkGroupConfigurationProperty(
+                result_configuration=athena.CfnWorkGroup.ResultConfigurationProperty(
+                    output_location="s3://hwe-athena-query-results/athena-query-results/"
+                )
+            )
+        )
+
+        # Create a Glue Data Catalog for Athena
+        database = glue.CfnDatabase(
+            self,
+            "hwe-glue-data-catalog", catalog_id = '153601099083',
+            database_input=glue.CfnDatabase.DatabaseInputProperty(
+                name="hwe"
+            ),
+        )
+
+
+
